86b6e28467caf2a4023bb9557a8eb3456d3e8643
ja153903 <jaimeabbariao@gmail.com>
2024-03-11 18:40:17 -0400

    feat: updating to use original

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -139,7 +139,7 @@ return {
     priority = 1000,
     config = function()
       vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_foreground = "original"
       vim.g.gruvbox_material_enable_bold = 1
     end,
   },
