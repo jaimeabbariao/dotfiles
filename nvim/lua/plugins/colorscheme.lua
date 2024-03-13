2e0d1961f3de92c74b53a18e44eab0dd1e437e37
ja153903 <jaimeabbariao@gmail.com>
2024-03-13 11:45:11 -0400

    feat: using solarized osaka

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -134,101 +134,10 @@ return {
     end,
   },
   {
-    "sainnhe/gruvbox-material",
-    enabled = false,
-    -- lazy = false,
-    -- priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_foreground = "mix"
-      vim.g.gruvbox_material_enable_bold = 1
-    end,
-  },
-  {
-    "marko-cerovac/material.nvim",
-    enabled = false,
-    -- lazy = false,
-    -- priority = 1000,
-    config = function()
-      vim.g.material_style = "darker"
-      require("material").setup({
-        contrast = {
-          terminal = false, -- Enable contrast for the built-in terminal
-          sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-          floating_windows = false, -- Enable contrast for floating windows
-          cursor_line = false, -- Enable darker background for the cursor line
-          non_current_windows = false, -- Enable contrasted background for non-current windows
-          filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
-        },
-        styles = { -- Give comments style such as bold, italic, underline etc.
-          comments = { --[[ italic = true ]]
-          },
-          strings = { --[[ bold = true ]]
-          },
-          keywords = { --[[ underline = true ]]
-          },
-          functions = { --[[ bold = true, undercurl = true ]]
-          },
-          variables = {},
-          operators = {},
-          types = {},
-        },
-        plugins = { -- Uncomment the plugins that you use to highlight them
-          -- Available plugins:
-          "dap",
-          -- "dashboard",
-          -- "eyeliner",
-          -- "fidget",
-          -- "flash",
-          "gitsigns",
-          -- "harpoon",
-          -- "hop",
-          -- "illuminate",
-          "indent-blankline",
-          -- "lspsaga",
-          "mini",
-          -- "neogit",
-          -- "neotest",
-          "neo-tree",
-          -- "neorg",
-          "noice",
-          -- "nvim-cmp",
-          "nvim-navic",
-          -- "nvim-tree",
-          "nvim-web-devicons",
-          -- "rainbow-delimiters",
-          -- "sneak",
-          "telescope",
-          "trouble",
-          "which-key",
-          "nvim-notify",
-        },
-
-        disable = {
-          colored_cursor = false, -- Disable the colored cursor
-          borders = false, -- Disable borders between verticaly split windows
-          background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
-          term_colors = false, -- Prevent the theme from setting terminal colors
-          eob_lines = false, -- Hide the end-of-buffer lines
-        },
-
-        high_visibility = {
-          lighter = false, -- Enable higher contrast text for lighter style
-          darker = false, -- Enable higher contrast text for darker style
-        },
-
-        lualine_style = "stealth", -- Lualine style ( can be 'stealth' or 'default' )
-        async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
-        custom_colors = function(colors)
-          colors.editor.fg = "#FFFFFF"
-        end,
-      })
-    end,
-  },
-  {
     "rose-pine/neovim",
-    lazy = false,
-    priority = 1000,
+    enabled = false,
+    -- lazy = false,
+    -- priority = 1000,
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
@@ -300,9 +209,49 @@ return {
     end,
   },
   {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("solarized-osaka").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = { bold = true },
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.5, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "solarized-osaka",
     },
   },
 }
