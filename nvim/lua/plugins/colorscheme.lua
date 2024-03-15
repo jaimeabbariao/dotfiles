be49fd6775f56e1df40e762503046400db3e495a
ja153903 <jaimeabbariao@gmail.com>
2024-03-14 21:07:18 -0400

    feat: creating my own nord scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,53 +1,5 @@
 return {
   {
-    "NTBBloodbath/doom-one.nvim",
-    enabled = false,
-    -- lazy = false,
-    -- priority = 1000,
-    config = function()
-      vim.g.doom_one_cursor_coloring = false
-      -- Set :terminal colors
-      vim.g.doom_one_terminal_colors = true
-      -- Enable italic comments
-      vim.g.doom_one_italic_comments = false
-      -- Enable TS support
-      vim.g.doom_one_enable_treesitter = true
-      -- Color whole diagnostic text or only underline
-      vim.g.doom_one_diagnostics_text_color = false
-      -- Enable transparent background
-      vim.g.doom_one_transparent_background = false
-
-      -- Pumblend transparency
-      vim.g.doom_one_pumblend_enable = true
-      vim.g.doom_one_pumblend_transparency = 20
-
-      -- Plugins integration
-      vim.g.doom_one_plugin_neorg = true
-      vim.g.doom_one_plugin_barbar = false
-      vim.g.doom_one_plugin_telescope = true
-      vim.g.doom_one_plugin_neogit = true
-      vim.g.doom_one_plugin_nvim_tree = true
-      vim.g.doom_one_plugin_dashboard = true
-      vim.g.doom_one_plugin_startify = true
-      vim.g.doom_one_plugin_whichkey = true
-      vim.g.doom_one_plugin_indent_blankline = true
-      vim.g.doom_one_plugin_vim_illuminate = true
-      vim.g.doom_one_plugin_lspsaga = true
-    end,
-  },
-  {
-    "Mofiqul/vscode.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = function()
-      return {
-        color_overrides = {
-          vscBack = "#161616",
-        },
-      }
-    end,
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     enabled = false,
@@ -206,10 +158,11 @@ return {
       })
     end,
   },
+  { "fcancelinha/northern.nvim", branch = "master", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vscode",
+      colorscheme = "northern",
     },
   },
 }
---
+++ wezterm/lua/nord.lua
@@ -0,0 +1,88 @@
+local M = {}
+
+local polar_night = {
+	nord0 = "#2E3440",
+	nord1 = "#3B4252",
+	nord2 = "#434C5E",
+	nord3 = "#4C566A",
+}
+
+local snow_storm = {
+	nord4 = "#D8DEE9",
+	nord5 = "#E5E9F0",
+	nord6 = "#ECEFF4",
+}
+
+local frost = {
+	nord7 = "#8FBCBB",
+	nord8 = "#88C0D0",
+	nord9 = "#81A1C1",
+	nord10 = "#5E81AC",
+}
+
+local aurora = {
+	red = "#BF616A",
+	orange = "#D08770",
+	yellow = "#EBCB8B",
+	green = "#A3BE8C",
+	purple = "#B48EAD",
+}
+
+local active_tab = {
+	bg_color = polar_night.nord0,
+	fg_color = snow_storm.nord4,
+}
+
+local inactive_tab = {
+	bg_color = polar_night.nord3,
+	fg_color = snow_storm.nord6,
+}
+
+function M.colors()
+	return {
+		foreground = snow_storm.nord4,
+		background = polar_night.nord0,
+		cursor_bg = snow_storm.nord6,
+		cursor_fg = "#282828",
+		selection_bg = snow_storm.nord6,
+		selection_fg = polar_night.nord3,
+		ansi = {
+			polar_night.nord1,
+			aurora.red,
+			aurora.green,
+			aurora.yellow,
+			frost.nord9,
+      aurora.purple,
+			frost.nord8,
+			snow_storm.nord5,
+		},
+		brights = {
+			polar_night.nord3,
+			aurora.red,
+			aurora.green,
+			aurora.yellow,
+			frost.nord9,
+			aurora.purple,
+			frost.nord7,
+			snow_storm.nord6,
+		},
+		tab_bar = {
+			background = polar_night.nord0,
+			active_tab = active_tab,
+			inactive_tab = inactive_tab,
+			inactive_tab_hover = active_tab,
+			new_tab = inactive_tab,
+			new_tab_hover = active_tab,
+      inactive_tab_edge = polar_night.nord2
+		},
+	}
+end
+
+function M.window_frame()
+  return {
+    active_titlebar_bg = polar_night.nord0,
+    inactive_titlebar_bg = polar_night.nord0
+  }
+end
+
+return M
