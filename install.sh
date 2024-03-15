318ac9d2bb65b8d1ea255f0a0efe29b87c1d2313
ja153903 <jaimeabbariao@gmail.com>
2024-03-15 10:42:38 -0400

    feat: creating small script for installation

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ">>> Installing Starship"
+brew install starship
+
+echo ">>> Installing Neovim"
+brew install neovim
+
+echo ">>> Installing ripgrep"
+brew install ripgrep
+
+echo ">>> Installing fd"
+brew install fd
+
+echo ">>> Installing lazygit"
+brew install jesseduffield/lazygit/lazygit
