24ccfbf45ce9e02af5ae400b4db2561875d6cbdd
jaimeabbariao <jabbariao@figma.com>
2024-03-19 19:16:09 -0400

    feat: simplifying how I use telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,15 +1,6 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    keys = {
-      {
-        "<leader>sg",
-        function()
-          require("telescope").extensions.live_grep_args.live_grep_args()
-        end,
-        desc = "Extended search with grep",
-      },
-    },
     dependencies = {
       "nvim-telescope/telescope-live-grep-args.nvim",
       -- build = "make",
