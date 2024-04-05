a37c98f1a6bce75bf335f10f212453e3338aacce
jaimeabbariao <jabbariao@figma.com>
2024-04-05 12:03:30 -0700

    feat: adding live grep args
    this way i can also filter by file type and dirs easily

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,10 +3,13 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-telescope/telescope-live-grep-args.nvim",
-      -- build = "make",
-      config = function()
-        require("telescope").load_extension("live_grep_args")
-      end,
+      version = "^1.0.0",
     },
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+    end,
+    keys = {
+      {"<leader>sg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>"}
+    }
   },
 }
