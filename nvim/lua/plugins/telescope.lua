a4f424f73f950499a41dca2b1d22d6b53ac3a642
jaimeabbariao <jabbariao@figma.com>
2024-04-22 11:07:41 -0400

    updating telescope config so I can search through hidden files as well

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,14 @@
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -10,6 +21,19 @@ return {
     end,
     keys = {
       {"<leader>sg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>"}
+    },
+    opts = {
+      defaults = {
+        vimgrep_arguments = vimgrep_arguments
+      },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+        },
+        git_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+        }
+      }
     }
   },
 }
