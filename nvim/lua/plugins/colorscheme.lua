9fcb3c6237a75ab0e16e855e93a9350245ff4433
jaimeabbariao <jabbariao@figma.com>
2024-04-30 01:37:22 -0400

    updating with modus vivendi

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -72,82 +72,85 @@ return {
       })
       vim.o.termguicolors = true
     end,
-  }, {
-  "rose-pine/neovim",
-  lazy = false,
-  priority = 1000,
-  name = "rose-pine",
-  opts = {
-    variant = "moon",      -- auto, main, moon, or dawn
-    dark_variant = "moon", -- main, moon, or dawn
-    dim_inactive_windows = false,
-    extend_background_behind_borders = true,
+  },
+  {
+    "rose-pine/neovim",
+    -- lazy = false,
+    -- priority = 1000,
+    enabled = false,
+    name = "rose-pine",
+    opts = {
+      variant = "moon",    -- auto, main, moon, or dawn
+      dark_variant = "moon", -- main, moon, or dawn
+      dim_inactive_windows = false,
+      extend_background_behind_borders = true,
 
-    enable = {
-      terminal = true,
-      legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-      migrations = true,        -- Handle deprecated options automatically
-    },
+      enable = {
+        terminal = true,
+        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+        migrations = true,      -- Handle deprecated options automatically
+      },
 
-    styles = {
-      bold = true,
-      italic = false,
-      transparency = false,
-    },
+      styles = {
+        bold = true,
+        italic = false,
+        transparency = false,
+      },
 
-    groups = {
-      border = "muted",
-      link = "iris",
-      panel = "surface",
+      groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
 
-      error = "love",
-      hint = "iris",
-      info = "foam",
-      note = "pine",
-      todo = "rose",
-      warn = "gold",
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        note = "pine",
+        todo = "rose",
+        warn = "gold",
 
-      git_add = "foam",
-      git_change = "rose",
-      git_delete = "love",
-      git_dirty = "rose",
-      git_ignore = "muted",
-      git_merge = "iris",
-      git_rename = "pine",
-      git_stage = "iris",
-      git_text = "rose",
-      git_untracked = "subtle",
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
 
-      h1 = "iris",
-      h2 = "foam",
-      h3 = "rose",
-      h4 = "gold",
-      h5 = "pine",
-      h6 = "foam",
-    },
+        h1 = "iris",
+        h2 = "foam",
+        h3 = "rose",
+        h4 = "gold",
+        h5 = "pine",
+        h6 = "foam",
+      },
 
-    highlight_groups = {
-      Comment = { fg = "foam" },
-      VertSplit = { fg = "muted", bg = "muted" },
-    },
+      highlight_groups = {
+        Comment = { fg = "foam" },
+        VertSplit = { fg = "muted", bg = "muted" },
+      },
 
-    before_highlight = function(group, highlight, palette)
-      -- Disable all undercurls
-      if highlight.undercurl then
+      before_highlight = function(group, highlight, palette)
+        -- Disable all undercurls
+        if highlight.undercurl then
           highlight.undercurl = false
-      end
+        end
 
-      -- Change palette colour
-      -- if highlight.fg == palette.pine then
-      --     highlight.fg = palette.foam
-      -- end
-    end,
-  }
-},
+        -- Change palette colour
+        -- if highlight.fg == palette.pine then
+        --     highlight.fg = palette.foam
+        -- end
+      end,
+    }
+  },
+  { "miikanissi/modus-themes.nvim", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "modus_vivendi",
     },
   },
 }
