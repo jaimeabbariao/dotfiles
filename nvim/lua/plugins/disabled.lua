569735640a53dbb52eba8626ca10e1d17ca7d799
jaimeabbariao <jabbariao@figma.com>
2024-05-07 13:42:34 -0400

    fixing something that wont work

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,5 +2,4 @@ return {
   { "nvimdev/dashboard-nvim",  enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/tokyonight.nvim",   enabled = false },
-  { "nvim-telescope/telescope.nvim", enabled = false }
 }
--- nvim/lua/plugins/fzf_lua.lua
+++ nvim/lua/plugins/fzf_lua.lua
@@ -2,9 +2,6 @@ return {
   {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      {"<leader><leader>", ":FzfLua<CR>"}
-    },
     config = function()
       require("fzf-lua").setup({})
     end
