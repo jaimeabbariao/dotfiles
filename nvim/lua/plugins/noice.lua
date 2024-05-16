aa055007eaa84cb0cbe449adb15f234a0fa78a29
jaimeabbariao <jabbariao@figma.com>
2024-05-16 10:18:53 -0400

    respecting noice again

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -27,9 +27,9 @@ return {
         -- NOTE: If you enable messages, then the cmdline is enabled automatically.
         -- This is a current Neovim limitation.
         enabled = true, -- enables the Noice messages UI
-        view = "mini", -- default view for messages
-        view_error = "mini", -- view for errors
-        view_warn = "mini", -- view for warnings
+        view = "notify", -- default view for messages
+        view_error = "notify", -- view for errors
+        view_warn = "notify", -- view for warnings
         view_history = "messages", -- view for :messages
         view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
       },
@@ -95,7 +95,7 @@ return {
         -- event is always "notify" and kind can be any log level as a string
         -- The default routes will forward notifications to nvim-notify
         -- Benefit of using Noice for this is the routing and consistent history view
-        enabled = false,
+        enabled = true,
         view = "notify",
       },
       lsp = {
