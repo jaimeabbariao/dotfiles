20f2c6cf39097512e0647bcdef21934df016dbf4
jaimeabbariao <jabbariao@figma.com>
2024-05-17 10:00:05 -0400

    updating some config related stuff

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,8 +11,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
-config.font = wezterm.font_with_fallback({ "MonoLisa", "nonicons" })
-config.font_size = 13
+config.font = wezterm.font_with_fallback({ "Berkeley Mono", "nonicons" })
+config.font_size = 14
 config.enable_scroll_bar = false
 config.window_padding = {
   left = 0,
@@ -25,7 +25,7 @@ config.tab_bar_at_bottom = true
 config.tab_max_width = 64
 -- config.colors = colors
 -- config.window_frame = window_frame
-config.line_height = 1.2
+config.line_height = 1.4
 -- config.color_scheme = 'One Dark (Gogh)'
 
 -- local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
