a2c73295b8e04b363bc83e70633c3524114a7d16
jaimeabbariao <jabbariao@figma.com>
2024-05-28 11:16:27 -0400

    updated

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -104,9 +104,9 @@ return {
 
       --- You can override specific highlights to use other groups or a hex color
       --- function will be called with a Highlights and ColorScheme table
-      ---@param highlights Highlights
-      ---@param colors ColorScheme
-      on_highlights = function(highlights, colors) end,
+      ---@param hl Highlights
+      ---@param c ColorScheme
+      on_highlights = function(hl, c) end,
     },
   },
   {
