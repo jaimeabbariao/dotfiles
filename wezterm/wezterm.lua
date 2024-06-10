9e01cbbfd2e4d6d2a83acfe6ccb47cf61b2cab52
jaimeabbariao <jabbariao@figma.com>
2024-06-10 10:52:23 -0400

    updating my config again

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- local rose_pine_main = require('lua/rose-pine-official').main
--- local colors = rose_pine_main.colors()
--- local window_frame = rose_pine_main.window_frame()
 -- local colors = require('lua/rose-pine').colors()
 -- local window_frame = require('lua/rose-pine').window_frame()
 
@@ -13,6 +10,7 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.font = wezterm.font_with_fallback({ "Berkeley Mono", "nonicons" })
 config.font_size = 14
+config.freetype_load_flags = "NO_HINTING"
 config.enable_scroll_bar = false
 config.window_padding = {
 	left = 0,
@@ -25,36 +23,39 @@ config.tab_bar_at_bottom = true
 config.tab_max_width = 64
 -- config.colors = colors
 -- config.window_frame = window_frame
-config.line_height = 1.2
--- config.color_scheme = 'One Dark (Gogh)'
+config.line_height = 1.4
+-- config.color_scheme = "DoomOne"
+config.color_scheme = "Solarized Dark - Patched"
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
 
--- config.color_scheme = 'Solarized Dark - Patched'
-config.window_background_opacity = 1.0
+local custom = wezterm.color.get_builtin_schemes()["Solarized Dark - Patched"]
+custom.background = "#00141A"
+-- custom.background = "#000"
+custom.foreground = "#FFF"
+custom.tab_bar = {
+	background = "#00141A",
+	active_tab = {
+		bg_color = "#063642",
+		fg_color = "#FFF",
+	},
+	inactive_tab = {
+		bg_color = "#00141A",
+		fg_color = "#FFF",
+	},
+	new_tab = {
+		bg_color = "#002B36",
+		fg_color = "#EEE8D5",
+	},
+}
 
--- Solarized Osaka
--- local custom = wezterm.color.get_builtin_schemes()["Solarized Dark - Patched"]
--- local custom = wezterm.color.get_builtin_schemes()["Modus Operandi (Gogh)"]
--- config.color_scheme = "Modus Operandi (Gogh)"
--- custom.background = "#00141A"
--- custom.foreground = "#FFF"
--- custom.tab_bar = {
--- 	background = "#00141A",
--- 	active_tab = {
--- 		bg_color = "#063642",
--- 		fg_color = "#FFF",
--- 	},
--- 	inactive_tab = {
--- 		bg_color = "#00141A",
--- 		fg_color = "#FFF",
--- 	},
--- 	new_tab = {
--- 		bg_color = "#002B36",
--- 		fg_color = "#EEE8D5",
--- 	},
--- }
+config.color_schemes = { ["Solarized Osaka"] = custom }
+config.color_scheme = "Solarized Osaka"
 
--- config.color_schemes = { ["Modus Operandi Mine"] = custom }
--- config.color_scheme = "Modus Operandi Mine"
+config.inactive_pane_hsb = {
+	saturation = 1.0,
+	brightness = 1.0,
+}
 
 -- local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
 -- custom.background = "#040404"
@@ -62,13 +63,13 @@ config.window_background_opacity = 1.0
 -- custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
 -- custom.tab_bar.new_tab.bg_color = "#080808"
 --
--- config.color_schemes = { ["OLEDCat"] = custom }
--- config.color_scheme = "OLEDCat"
+-- config.color_schemes = {
+-- 	["OLEDppuccin"] = custom,
+-- }
+-- config.color_scheme = "OLEDppuccin"
 
 -- timeout_milliseconds defaults to 1000 and can be omitted
--- config.color_scheme = 'Moonfly'
-
-config.leader = { key = "e", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	{
 		key = "n",
