a20b007831e3d56e0af03f653590277b93086b29
jaimeabbariao <jabbariao@figma.com>
2024-06-17 12:18:14 -0400

    update wezterm

---
+++ wezterm/lua/solarized-osaka.lua
@@ -0,0 +1,25 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+local custom = wezterm.color.get_builtin_schemes()['Solarized Dark - Patched']
+custom.background = '#00141A'
+custom.foreground = '#FFF'
+custom.tab_bar = {
+  background = '#00141A',
+  active_tab = {
+    bg_color = '#063642',
+    fg_color = '#FFF',
+  },
+  inactive_tab = {
+    bg_color = '#00141A',
+    fg_color = '#FFF',
+  },
+  new_tab = {
+    bg_color = '#002B36',
+    fg_color = '#EEE8D5',
+  },
+}
+
+return {
+  custom = custom,
+}
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- local colors = require('lua/rose-pine').colors()
--- local window_frame = require('lua/rose-pine').window_frame()
+local solarized_osaka = require('lua/solarized-osaka').custom
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -21,54 +20,17 @@ config.window_padding = {
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.tab_max_width = 64
--- config.colors = colors
--- config.window_frame = window_frame
 config.line_height = 1.4
--- config.color_scheme = "DoomOne"
--- config.color_scheme = 'Solarized Dark - Patched'
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
 
-local custom = wezterm.color.get_builtin_schemes()['Solarized Dark - Patched']
--- local custom = wezterm.color.get_builtin_schemes()['Monokai (base16)']
-custom.background = '#00141A'
--- custom.background = "#000"
--- custom.background = '#272821'
-custom.foreground = '#FFF'
-custom.tab_bar = {
-  background = '#00141A',
-  active_tab = {
-    bg_color = '#063642',
-    fg_color = '#FFF',
-  },
-  inactive_tab = {
-    bg_color = '#00141A',
-    fg_color = '#FFF',
-  },
-  new_tab = {
-    bg_color = '#002B36',
-    fg_color = '#EEE8D5',
-  },
-}
-
-config.color_schemes = { ['Solarized Osaka'] = custom }
+config.color_schemes = { ['Solarized Osaka'] = solarized_osaka }
 config.color_scheme = 'Solarized Osaka'
 
 config.inactive_pane_hsb = {
   saturation = 1.0,
   brightness = 1.0,
 }
-
--- local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
--- custom.background = "#040404"
--- custom.tab_bar.background = "#040404"
--- custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
--- custom.tab_bar.new_tab.bg_color = "#080808"
---
--- config.color_schemes = {
--- 	["OLEDppuccin"] = custom,
--- }
--- config.color_scheme = "OLEDppuccin"
 
 -- timeout_milliseconds defaults to 1000 and can be omitted
 config.leader = { key = 'e', mods = 'CTRL', timeout_milliseconds = 1000 }
