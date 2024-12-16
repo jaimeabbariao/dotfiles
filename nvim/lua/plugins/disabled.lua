8de15a48ff34ab046c4158bc161f5497a1d881af
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2024-12-16 10:27:12 -0500

    updating stuff

--- nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "nvimdev/dashboard-nvim",  enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/tokyonight.nvim",   enabled = false },
-  { "catppuccin/nvim",         enabled = false }
-}
