603085202e9f55f95e8a9494a4e74eb01f8a9445
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-01-23 16:27:16 -0500

    updating ghostty config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -237,7 +237,8 @@ return {
   },
   {
     "ellisonleao/gruvbox.nvim",
-    priority = 1000,
+    -- priority = 1000,
+    enabled = false,
     config = function()
       require("gruvbox").setup({
         terminal_colors = true, -- add neovim terminal colors
@@ -267,6 +268,47 @@ return {
       })
 
       vim.cmd([[colorscheme gruvbox]])
+    end,
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = false,
+    priority = 1000,
+    -- enabled = false
+    config = function()
+      local c = require("vscode.colors").get_colors()
+      require("vscode").setup({
+        -- Alternatively set style in setup
+        -- style = 'light'
+
+        -- Enable transparent background
+        transparent = false,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Underline `@markup.link.*` variants
+        underline_links = true,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Override colors (see ./lua/vscode/colors.lua)
+        color_overrides = {
+          vscLineNumber = "#FFFFFF",
+        },
+
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+          -- this supports the same val table as vim.api.nvim_set_hl
+          -- use colors from this colorscheme by requiring vscode.colors!
+          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+        },
+      })
+      -- require('vscode').load()
+
+      -- load the theme without affecting devicon colors.
+      vim.cmd.colorscheme("vscode")
     end,
   },
 }
