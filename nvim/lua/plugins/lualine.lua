749e4c9ef65a142dc811d49061d0ca823f743c09
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-01-31 12:27:37 -0500

    updating lualine to be simpler

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -29,9 +29,8 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch" },
-
         lualine_c = {
-          LazyVim.lualine.root_dir(),
+          -- LazyVim.lualine.root_dir({ icon = "" }),
           {
             "diagnostics",
             symbols = {
@@ -41,35 +40,10 @@ return {
               hint = icons.diagnostics.Hint,
             },
           },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { LazyVim.lualine.pretty_path() },
+          { LazyVim.lualine.pretty_path({ relative = "root" }) },
         },
         lualine_x = {
           Snacks.profiler.status(),
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            color = function() return { fg = Snacks.util.color("Statement") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = function() return { fg = Snacks.util.color("Constant") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return "  " .. require("dap").status() end,
-            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = function() return { fg = Snacks.util.color("Debug") } end,
-          },
-          -- stylua: ignore
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = function() return { fg = Snacks.util.color("Special") } end,
-          },
           {
             "diff",
             symbols = {
@@ -89,10 +63,7 @@ return {
             end,
           },
         },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
+        lualine_y = {},
         lualine_z = {},
       },
       extensions = { "neo-tree", "lazy" },
