3b78bb078a890ff1c00d9ce4c7f5957c30f17105
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-07-01 22:40:43 -0400

    updating config again

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,54 +1,14 @@
 return {
+  -- Lazy
   {
-    "ribru17/bamboo.nvim",
-    lazy = false,
-    priority = 1000,
+    "vague2k/vague.nvim",
     config = function()
-      -- Lua
-      require("bamboo").setup({
-        -- Main options --
-        -- NOTE: to use the light theme, set `vim.o.background = 'light'`
-        style = "vulgaris", -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
-        toggle_style_key = nil, -- Keybind to toggle theme style. Leave it nil to disable it, or set it to a string, e.g. "<leader>ts"
-        toggle_style_list = { "vulgaris", "multiplex", "light" }, -- List of styles to toggle between
-        transparent = false, -- Show/hide background
-        dim_inactive = false, -- Dim inactive windows/buffers
-        term_colors = true, -- Change terminal color as per the selected theme style
-        ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-        cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-        -- Change code style ---
-        -- Options are anything that can be passed to the `vim.api.nvim_set_hl` table
-        -- You can also configure styles with a string, e.g. keywords = 'italic,bold'
-        code_style = {
-          comments = { italic = true },
-          conditionals = { italic = true },
-          keywords = {},
-          functions = {},
-          namespaces = { italic = true },
-          parameters = { italic = true },
-          strings = {},
-          variables = {},
-        },
-
-        -- Lualine options --
-        lualine = {
-          transparent = false, -- lualine center bar transparency
-        },
-
-        -- Custom Highlights --
-        colors = {}, -- Override default colors
-        highlights = {}, -- Override highlight groups
-
-        -- Plugins Config --
-        diagnostics = {
-          darker = false, -- darker colors for diagnostic
-          undercurl = true, -- use undercurl instead of underline for diagnostics
-          background = true, -- use background color for virtual text
-        },
+      -- NOTE: you do not need to call setup if you don't want to.
+      require("vague").setup({
+        -- optional configuration here
       })
-      -- Lua
-      require("bamboo").load()
+
+      vim.cmd([[colorscheme vague]])
     end,
   },
 }
