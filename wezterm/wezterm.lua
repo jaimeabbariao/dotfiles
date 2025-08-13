3cd2d0f50ce419f09c4dbb03b168084eaa94f518
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-08-13 10:14:54 -0400

    updating with some colorscheme and font changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,14 +6,15 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
 
-config.font = wezterm.font("MonoLisa")
+config.font = wezterm.font("Monaspace Krypton")
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
-config.color_scheme = "Gruvbox dark, hard (base16)"
 -- or, changing the font size and color scheme.
 config.font_size = 12
+
+config.color_scheme = "Vs Code Dark+ (Gogh)"
 
 -- Finally, return the configuration to wezterm:
 return config
