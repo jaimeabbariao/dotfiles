a734b586aff0e210522a1b14ff063d08b5cc511f
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-09-02 10:05:15 -0400

    updating some stuff again

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,40 +1,8 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      -- Default options:
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        inverse = false, -- invert background for search, diffs, statuslines and errors
-        contrast = "hard", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-      vim.cmd("colorscheme gruvbox")
-    end,
-  },
-  {
     "miikanissi/modus-themes.nvim",
-    enabled = false,
-    -- priority = 1000,
-    -- lazy = false,
+    priority = 1000,
+    lazy = false,
     config = function()
       require("modus-themes").setup({
         -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
@@ -69,7 +37,7 @@ return {
         on_highlights = function(highlights, colors) end,
       })
 
-      -- vim.cmd("colorscheme modus_operandi") -- or 'modus-operandi' for light mode
+      vim.cmd("colorscheme modus_operandi") -- or 'modus-operandi' for light mode
     end,
   },
   {
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,7 +16,7 @@ local config = wezterm.config_builder()
 -- config.font = wezterm.font_with_fallback({ "Gintronic", "Noto Color Emoji" })
 config.font = wezterm.font_with_fallback({ "mononoki", "Noto Color Emoji" })
 
-config.font_size = 16
+config.font_size = 14
 
 config.color_scheme = "Gruvbox dark, hard (base16)"
 
