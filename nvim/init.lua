2430ffdf173d3818244c10be59695c66496e9ca3
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-10-19 02:52:33 -0400

    updating stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,1 @@
-local function is_git_merge()
-  return vim.env.GIT_MERGE_FILE ~= nil or vim.env.GIT_INDEX_FILE ~= nil or vim.fn.expand("%:t"):match("^MERGE_") ~= nil
-end
-if not is_git_merge() then
-  require("config.lazy")
-end
+require("config.lazy")
