db7c4efbb1f96d9653adfa1dc12d207e43612f5b
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-10-29 23:03:14 -0400

    using a light theme again

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,46 @@
 return {
+  {
+    "miikanissi/modus-themes.nvim",
+    priority = 1000,
+    config = function()
+      vim.o.background = "light"
+
+      -- Default options
+      require("modus-themes").setup({
+        -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+        -- `auto` will automatically set style based on background set with vim.o.background
+        style = "auto",
+        variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+        transparent = false, -- Transparent background (as supported by the terminal)
+        dim_inactive = false, -- "non-current" windows are dimmed
+        hide_inactive_statusline = false, -- Hide statuslines on inactive windows. Works with the standard **StatusLine**, **LuaLine** and **mini.statusline**
+        line_nr_column_background = true, -- Distinct background colors in line number column. `false` will disable background color and fallback to Normal background
+        sign_column_background = true, -- Distinct background colors in sign column. `false` will disable background color and fallback to Normal background
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+        },
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- Function will be called with a ColorScheme table
+        --- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- Function will be called with a Highlights and ColorScheme table
+        --- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
+      })
+      vim.cmd([[colorscheme modus]]) -- modus_operandi, modus_vivendi
+    end,
+  },
   -- {
   --   "craftzdog/solarized-osaka.nvim",
   --   lazy = false,
@@ -42,6 +84,7 @@ return {
   -- },
   {
     "EdenEast/nightfox.nvim",
+    enabled = false,
     config = function()
       -- Default options
       require("nightfox").setup({
@@ -89,7 +132,7 @@ return {
       })
 
       -- setup must be called before loading
-      vim.cmd("colorscheme carbonfox")
+      -- vim.cmd("colorscheme carbonfox")
     end,
   }, -- lazy
 }
