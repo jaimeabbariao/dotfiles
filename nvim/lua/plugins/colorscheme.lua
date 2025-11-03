7b60aebc69e2d3fbaf58453d2e2f19c856e73e0d
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-11-02 22:23:42 -0500

    updating

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,90 +1,6 @@
 return {
   {
-    "miikanissi/modus-themes.nvim",
-    priority = 1000,
-    config = function()
-      vim.o.background = "light"
-
-      -- Default options
-      require("modus-themes").setup({
-        -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
-        -- `auto` will automatically set style based on background set with vim.o.background
-        style = "auto",
-        variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
-        transparent = false, -- Transparent background (as supported by the terminal)
-        dim_inactive = false, -- "non-current" windows are dimmed
-        hide_inactive_statusline = false, -- Hide statuslines on inactive windows. Works with the standard **StatusLine**, **LuaLine** and **mini.statusline**
-        line_nr_column_background = false, -- Distinct background colors in line number column. `false` will disable background color and fallback to Normal background
-        sign_column_background = false, -- Distinct background colors in sign column. `false` will disable background color and fallback to Normal background
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-        },
-
-        --- You can override specific color groups to use other groups or a hex color
-        --- Function will be called with a ColorScheme table
-        --- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the ColorScheme table
-        ---@param colors ColorScheme
-        on_colors = function(colors) end,
-
-        --- You can override specific highlights to use other groups or a hex color
-        --- Function will be called with a Highlights and ColorScheme table
-        --- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the Highlights and ColorScheme table
-        ---@param highlights Highlights
-        ---@param colors ColorScheme
-        on_highlights = function(highlights, colors) end,
-      })
-      vim.cmd([[colorscheme modus]]) -- modus_operandi, modus_vivendi
-    end,
-  },
-  -- {
-  --   "craftzdog/solarized-osaka.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("solarized-osaka").setup({
-  --       -- your configuration comes here
-  --       -- or leave it empty to use the default settings
-  --       transparent = false, -- Enable this to disable setting the background color
-  --       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-  --       styles = {
-  --         -- Style to be applied to different syntax groups
-  --         -- Value is any valid attr-list value for `:help nvim_set_hl`
-  --         comments = { italic = true },
-  --         keywords = { italic = true },
-  --         functions = {},
-  --         variables = {},
-  --         -- Background styles. Can be "dark", "transparent" or "normal"
-  --         sidebars = "dark", -- style for sidebars, see below
-  --         floats = "dark", -- style for floating windows
-  --       },
-  --       sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  --       day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  --       hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  --       dim_inactive = false, -- dims inactive windows
-  --       lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-  --
-  --       --- You can override specific color groups to use other groups or a hex color
-  --       --- function will be called with a ColorScheme table
-  --       ---@param colors ColorScheme
-  --       on_colors = function(colors) end,
-  --
-  --       --- You can override specific highlights to use other groups or a hex color
-  --       --- function will be called with a Highlights and ColorScheme table
-  --       ---@param highlights Highlights
-  --       ---@param colors ColorScheme
-  --       on_highlights = function(highlights, colors) end,
-  --     })
-  --     vim.cmd("colorscheme solarized-osaka")
-  --   end,
-  -- },
-  {
     "EdenEast/nightfox.nvim",
-    enabled = false,
     config = function()
       -- Default options
       require("nightfox").setup({
@@ -132,7 +48,7 @@ return {
       })
 
       -- setup must be called before loading
-      -- vim.cmd("colorscheme carbonfox")
+      vim.cmd("colorscheme carbonfox")
     end,
   }, -- lazy
 }
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -8,28 +8,28 @@ return {
       -- -- your configuration comes here
       -- -- or leave it empty to use the default settings
       -- -- refer to the configuration section below
-      -- bigfile = { enabled = true },
-      -- dashboard = { enabled = false },
+      bigfile = { enabled = true },
+      dashboard = { enabled = false },
       indent = { enabled = false },
-      -- input = { enabled = true },
+      input = { enabled = true },
       notifier = { enabled = false },
-      -- quickfile = { enabled = true },
-      -- scroll = { enabled = true },
-      -- statuscolumn = { enabled = true },
-      -- words = { enabled = true },
-      -- picker = {
-      --   layout = {
-      --     fullscreen = true,
-      --   },
-      --   sources = {
-      --     explorer = {
-      --       auto_close = true,
-      --     },
-      --   },
-      -- },
-      -- scope = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      picker = {
+        layout = {
+          fullscreen = true,
+        },
+        sources = {
+          explorer = {
+            auto_close = true,
+          },
+        },
+      },
+      scope = { enabled = true },
       animate = { enabled = true },
-      -- image = { enabled = false },
+      image = { enabled = false },
     },
   },
 }
