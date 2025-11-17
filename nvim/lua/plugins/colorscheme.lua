5c7071686bc7b94d41bb0910f362dc4e64c0f8da
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-11-17 15:48:58 -0500

    updating stuff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,28 @@
 return {
   {
+    "tiesen243/vercel.nvim",
+    enabled = false,
+    -- lazy = false,
+    -- priority = 1000,
+    config = function()
+      require("vercel").setup({
+        transparent = true, -- Boolean: Sets the background to transparent (Default: false)
+        italics = {
+          comments = false, -- Boolean: Italicizes comments (Default: true)
+          keywords = false, -- Boolean: Italicizes keywords (Default: true)
+          functions = false, -- Boolean: Italicizes functions (Default: true)
+          strings = false, -- Boolean: Italicizes strings (Default: true)
+          variables = false, -- Boolean: Italicizes variables (Default: true)
+          bufferline = false, -- Boolean: Italicizes bufferline (Default: false)
+        },
+        overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+      })
+
+      -- This must be called before setting the colorscheme, otherwise it will always default to light mode
+      -- vim.cmd.colorscheme("vercel")
+    end,
+  },
+  {
     "EdenEast/nightfox.nvim",
     enabled = false,
     config = function()
@@ -99,11 +122,44 @@ return {
     end,
   },
   {
-    "wurli/cobalt.nvim",
+    "craftzdog/solarized-osaka.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme cobalt]])
+      require("solarized-osaka").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = false },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "transparent", -- style for sidebars, see below
+          floats = "transparent", -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.8, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
+      })
+      vim.cmd.colorscheme("solarized-osaka")
     end,
   },
 }
