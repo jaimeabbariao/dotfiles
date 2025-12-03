a1ad35453adf69d36c4b8e07bbd3f1aacea9758b
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-12-03 10:13:14 -0500

    updating with alacritty setup

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,145 +1,22 @@
+local opts = {
+  theme = "dark",
+  styles = {
+    type = { bold = true },
+    lsp = { underline = true },
+    match_paren = { underline = true },
+  },
+}
+
+local function config()
+  local plugin = require("no-clown-fiesta")
+  local loaded_state = plugin.load(opts)
+  vim.cmd([[colorscheme no-clown-fiesta]])
+  return loaded_state
+end
+
 return {
-  {
-    "EdenEast/nightfox.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-    enabled = false,
-    config = function()
-      -- Default options
-      require("nightfox").setup({
-        options = {
-          -- Compiled file's destination location
-          compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-          compile_file_suffix = "_compiled", -- Compiled file suffix
-          transparent = true, -- Disable setting background
-          terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-          dim_inactive = false, -- Non focused panes set to alternative background
-          module_default = true, -- Default enable value for modules
-          colorblind = {
-            enable = false, -- Enable colorblind support
-            simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
-            severity = {
-              protan = 0, -- Severity [0,1] for protan (red)
-              deutan = 0, -- Severity [0,1] for deutan (green)
-              tritan = 0, -- Severity [0,1] for tritan (blue)
-            },
-          },
-          styles = { -- Style to be applied to different syntax groups
-            comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
-            conditionals = "NONE",
-            constants = "NONE",
-            functions = "NONE",
-            keywords = "NONE",
-            numbers = "NONE",
-            operators = "NONE",
-            strings = "NONE",
-            types = "NONE",
-            variables = "NONE",
-          },
-          inverse = { -- Inverse highlight for different types
-            match_paren = false,
-            visual = false,
-            search = false,
-          },
-          modules = { -- List of various plugins and additional options
-            -- ...
-          },
-        },
-        palettes = {},
-        specs = {},
-        groups = {},
-      })
-
-      -- setup must be called before loading
-      -- vim.cmd("colorscheme dayfox")
-    end,
-  }, -- lazy
-  {
-    "Mofiqul/vscode.nvim",
-    -- enabled = false,
-    lazy = false,
-    config = function()
-      local c = require("vscode.colors").get_colors()
-      require("vscode").setup({
-        -- Alternatively set style in setup
-        style = "light",
-
-        -- Enable transparent background
-        transparent = false,
-
-        -- Enable italic comment
-        italic_comments = true,
-
-        -- Enable italic inlay type hints
-        italic_inlayhints = true,
-
-        -- Underline `@markup.link.*` variants
-        underline_links = true,
-
-        -- Disable nvim-tree background color
-        disable_nvimtree_bg = true,
-
-        -- Apply theme colors to terminal
-        terminal_colors = true,
-
-        -- Override colors (see ./lua/vscode/colors.lua)
-        color_overrides = {
-          vscLineNumber = "#FFFFFF",
-        },
-
-        -- Override highlight groups (see ./lua/vscode/theme.lua)
-        group_overrides = {
-          -- this supports the same val table as vim.api.nvim_set_hl
-          -- use colors from this colorscheme by requiring vscode.colors!
-          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-        },
-      })
-      -- require('vscode').load()
-
-      -- load the theme without affecting devicon colors.
-      vim.cmd.colorscheme("vscode")
-    end,
-  },
-  {
-    "craftzdog/solarized-osaka.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-    enabled = false,
-    config = function()
-      require("solarized-osaka").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        transparent = true, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = true },
-          keywords = { italic = false },
-          functions = {},
-          variables = {},
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "transparent", -- style for sidebars, see below
-          floats = "transparent", -- style for floating windows
-        },
-        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.8, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false, -- dims inactive windows
-        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-        --- You can override specific color groups to use other groups or a hex color
-        --- function will be called with a ColorScheme table
-        ---@param colors ColorScheme
-        on_colors = function(colors) end,
-
-        --- You can override specific highlights to use other groups or a hex color
-        --- function will be called with a Highlights and ColorScheme table
-        ---@param highlights Highlights
-        ---@param colors ColorScheme
-        on_highlights = function(highlights, colors) end,
-      })
-      -- vim.cmd.colorscheme("solarized-osaka")
-    end,
-  },
+  "aktersnurra/no-clown-fiesta.nvim",
+  priority = 1000,
+  config = config,
+  lazy = false,
 }
