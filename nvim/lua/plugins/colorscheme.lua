c72f84b9f1cf7e86359372f81ba03764ef8f9efb
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-02-17 20:49:23 -0500

    updating stuff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,24 +3,35 @@ return {
   name = "evergarden",
   priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
   lazy = false,
-  opts = {
-    theme = {
-      variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
-      accent = "green",
-    },
-    editor = {
-      transparent_background = false,
-      sign = { color = "none" },
-      float = {
-        color = "mantle",
-        solid_border = false,
-      },
-      completion = {
-        color = "surface0",
-      },
-    },
-  },
   config = function()
+    require("evergarden").setup({
+      theme = {
+        variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+        accent = "green",
+      },
+      editor = {
+        transparent_background = false,
+        override_terminal = true,
+        sign = { color = "none" },
+        float = {
+          color = "none",
+          solid_border = false,
+        },
+        completion = {
+          color = "surface0",
+        },
+      },
+      style = {
+        tabline = { "reverse" },
+        search = { "italic", "reverse" },
+        incsearch = { "italic", "reverse" },
+        types = { "italic" },
+        keyword = { "italic" },
+        comment = { "italic" },
+      },
+      overrides = {},
+      color_overrides = {},
+    })
     vim.cmd([[colorscheme evergarden]])
   end,
 }
