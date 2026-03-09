06ae72a7a343e35457982fe441c12092cdb139f2
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-03-09 12:04:32 -0400

    updating stuff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,37 +1,52 @@
 return {
-  "everviolet/nvim",
-  name = "evergarden",
-  priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-  lazy = false,
-  config = function()
-    require("evergarden").setup({
-      theme = {
-        variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
-        accent = "green",
+  {
+    "craftzdog/solarized-osaka.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    enabled = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
       },
-      editor = {
-        transparent_background = false,
-        override_terminal = true,
-        sign = { color = "none" },
-        float = {
-          color = "none",
-          solid_border = false,
-        },
-        completion = {
-          color = "surface0",
-        },
-      },
-      style = {
-        tabline = { "reverse" },
-        search = { "italic", "reverse" },
-        incsearch = { "italic", "reverse" },
-        types = { "italic" },
-        keyword = { "italic" },
-        comment = { "italic" },
-      },
-      overrides = {},
-      color_overrides = {},
-    })
-    vim.cmd([[colorscheme evergarden]])
-  end,
+      sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      day_brightness = 1.0, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+      --- You can override specific color groups to use other groups or a hex color
+      --- function will be called with a ColorScheme table
+      ---@param colors ColorScheme
+      on_colors = function(colors) end,
+
+      --- You can override specific highlights to use other groups or a hex color
+      --- function will be called with a Highlights and ColorScheme table
+      ---@param highlights Highlights
+      ---@param colors ColorScheme
+      on_highlights = function(highlights, colors) end,
+    },
+  },
+  {
+    "oskarnurm/koda.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "koda-moss",
+    },
+  },
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,47 +7,68 @@ return {
       },
       servers = {
         sorbet = {},
+        tsserver = { enabled = false },
+        vtsls = { enabled = false },
+        tsgo = {
+          cmd = { "tsgo", "--lsp", "--stdio" },
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          root_markers = {
+            "tsconfig.json",
+            "jsconfig.json",
+            "package.json",
+            ".git",
+            "tsconfig.base.json",
+          },
+          enabled = true,
+        },
       },
     },
   },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    config = function()
-      require("typescript-tools").setup({
-        settings = {
-          separate_diagnostic_server = true,
-          publish_diagnostic_on = "insert_leave",
-          expose_as_code_action = { "add_missing_imports", "remove_unused" },
-          tsserver_path = nil,
-          tsserver_plugins = {},
-          tsserver_max_memory = 16384,
-          tsserver_format_options = {
-            semicolons = "insert",
-            insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets = false,
-          },
-          tsserver_file_preferences = {
-            includeInlayParameterNameHints = "none",
-            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-            includeInlayFunctionParameterTypeHints = false,
-            includeInlayVariableTypeHints = false,
-            includeInlayPropertyDeclarationTypeHints = false,
-            includeInlayFunctionLikeReturnTypeHints = false,
-            includeInlayEnumMemberValueHints = false,
-            includeCompletionsForModuleExports = false,
-            includeCompletionsWithSnippetText = false,
-          },
-          tsserver_locale = "en",
-          complete_function_calls = false,
-          include_completions_with_insert_text = false,
-          code_lens = "off",
-          disable_member_code_lens = true,
-          jsx_close_tag = {
-            enable = false,
-            filetypes = { "javascriptreact", "typescriptreact" },
-          },
-        },
-      })
-    end,
-  },
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   config = function()
+  --     require("typescript-tools").setup({
+  --       settings = {
+  --         separate_diagnostic_server = true,
+  --         publish_diagnostic_on = "insert_leave",
+  --         expose_as_code_action = { "add_missing_imports", "remove_unused" },
+  --         tsserver_path = nil,
+  --         tsserver_plugins = {},
+  --         tsserver_max_memory = 16384,
+  --         tsserver_format_options = {
+  --           semicolons = "insert",
+  --           insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets = false,
+  --         },
+  --         tsserver_file_preferences = {
+  --           includeInlayParameterNameHints = "none",
+  --           includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+  --           includeInlayFunctionParameterTypeHints = false,
+  --           includeInlayVariableTypeHints = false,
+  --           includeInlayPropertyDeclarationTypeHints = false,
+  --           includeInlayFunctionLikeReturnTypeHints = false,
+  --           includeInlayEnumMemberValueHints = false,
+  --           includeCompletionsForModuleExports = false,
+  --           includeCompletionsWithSnippetText = false,
+  --         },
+  --         tsserver_locale = "en",
+  --         complete_function_calls = false,
+  --         include_completions_with_insert_text = false,
+  --         code_lens = "off",
+  --         disable_member_code_lens = true,
+  --         jsx_close_tag = {
+  --           enable = false,
+  --           filetypes = { "javascriptreact", "typescriptreact" },
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
 }
