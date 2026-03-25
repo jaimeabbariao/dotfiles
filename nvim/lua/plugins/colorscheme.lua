453fb753af267dc26dec1c3ab391ee9af0af0c74
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-03-25 13:17:10 -0400

    updating colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -86,9 +86,14 @@ return {
     },
   }, -- lazy
   {
+    "oskarnurm/koda.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "koda-light",
     },
   },
 }
