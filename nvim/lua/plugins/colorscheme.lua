7428b8071d31ce4015b882c50535c05800603474
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-03-26 13:42:47 -0400

    adding colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -87,13 +87,19 @@ return {
   }, -- lazy
   {
     "oskarnurm/koda.nvim",
+    enabled = false,
+    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    -- priority = 1000, -- make sure to load this before all the other start plugins
+  },
+  {
+    "Mofiqul/vscode.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "koda-light",
+      colorscheme = "vscode",
     },
   },
 }
