c5edc78c8c7b47d46c71dd5d6ae5871af6a7bf89
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-04-11 12:26:52 -0400

    chore: updating colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
   {
-    "oskarnurm/koda.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "RRethy/base16-nvim",
+    lazy = false,
+    priority = 1000,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "koda-moss",
+      colorscheme = "base16-windows-95",
     },
   },
 }
