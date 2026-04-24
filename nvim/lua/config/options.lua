3d22e723aaabed9e7f85eb84918a72d19a2798e9
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-04-24 13:10:57 -0400

    fix: clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,16 @@ vim.opt.list = false
 
 vim.opt.mousescroll = "ver:1,hor:0"
 vim.opt.wrap = false
+
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
