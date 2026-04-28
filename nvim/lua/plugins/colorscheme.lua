63e3313fc496074ac8cf4de4cab94855d4225d07
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-04-28 10:26:14 -0400

    fix: using coda

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,9 +4,14 @@ return {
   },
   { "EdenEast/nightfox.nvim" }, -- lazy
   {
+    "oskarnurm/koda.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
+      colorscheme = "koda",
     },
   },
 }
