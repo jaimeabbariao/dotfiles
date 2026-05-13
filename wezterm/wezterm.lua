c3d1405ca9021c2e5bcd53d8af8880633716b632
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-05-12 22:34:56 -0400

    feat: adding back wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,45 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("PlemolJP Console NF")
+config.font_size = 13
+
+-- keymaps for split pane, etc.
+config.leader = { key = "e", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "d",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+}
+
+config.use_fancy_tab_bar = false
+config.tab_max_width = 100
+
+return config
