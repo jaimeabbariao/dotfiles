de03daf584e4f4556d65dc173b576fbe00b1f322
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-06-02 09:42:03 -0400

    feat: updating tmux keybinds so that they dont conflict

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 local scheme = wezterm.color.load_scheme(wezterm.config_dir .. "/colors/modus_vivendi.toml")
 
-config.font = wezterm.font("TX-02")
+config.font = wezterm.font("MonoLisa")
 config.line_height = 1.2
 config.color_scheme_dirs = { wezterm.config_dir .. "/colors" }
 config.color_scheme = "Modus Vivendi"
