a21897022d9d8a97b207e5648f8ab92d88b00ae1
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-07-07 11:09:57 -0400

    feat: updating colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,17 @@
 return {
   {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
+    "oskarnurm/koda.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("koda").setup({ transparent = true })
+      vim.cmd("colorscheme koda-moss")
+    end,
   },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "solarized-osaka",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "solarized-osaka",
+  --   },
+  -- },
 }
