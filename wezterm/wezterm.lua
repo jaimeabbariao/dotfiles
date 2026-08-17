98afbe00a0a171f8a65ba0e503b0c23988ad9393
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-08-16 21:29:45 -0400

    feat: updating line height

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,7 +10,6 @@ local scheme = wezterm.color.load_scheme(wezterm.config_dir .. "/colors/modus_op
 config.font_dirs = { wezterm.home_dir .. "/Library/Fonts" }
 config.font = wezterm.font("PlemolJP Console NF")
 config.font_size = 14.0
-config.line_height = 1.4
 config.color_scheme_dirs = { wezterm.config_dir .. "/colors" }
 config.color_scheme = "embark"
 config.window_decorations = "RESIZE"
