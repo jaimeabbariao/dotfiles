45d7eed0c974fca6698a85352c003e9d7568b850
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-09-02 13:23:26 -0400

    feat: updating stuff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,14 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "embark-theme/vim",
     lazy = false,
     priority = 1000,
-    config = function()
-      -- Default options:
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "hard", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-    end,
+    name = "embark",
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "embark",
     },
   },
 }
