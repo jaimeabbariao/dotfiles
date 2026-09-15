51ccdc8b9ffa92c57678bf4ef6a67b2992b71144
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2026-09-14 21:34:50 -0400

    colorscheme: updating to use vscode light

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,13 @@
 return {
   { "datsfilipe/vesper.nvim", lazy = false, priority = 1000 },
-  { "Mofiqul/vscode.nvim", lazy = false, priority = 1000 },
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = "light"
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
